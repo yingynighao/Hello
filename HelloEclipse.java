321a6d99669cf90be83680402b659b701d07ae0a
yingyinghao <eisenying@gmail.com>
2021-01-10 13:43:49 +0800

    add123

---
+++ HelloEclipse.java
@@ -0,0 +1,23 @@
+package xxx;
+
+public class HelloEclipse {
+/*aaa*/
+	
+	public static void main(String[] args) {
+//		System.out.println("Hello Eclipse");
+//		System.out.println("hello");	
+//		
+		final int num = 1;
+		//Java為強行別語言
+
+		final int Num;
+		Num = 10;//如果把num 宣告為常數時，此敘述有問題
+		
+		
+//		int a,b,c;
+//		a= 12;
+//		System.out.println(num);
+//		System.out.println(Num);
+//		
+    }
+}
